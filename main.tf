b2abf875023a252f14d9ccba53f438056fe97d0e
bob1983 <bob.1983.g@gmail.com>
2017-07-07 17:42:55 +0900

    Make a create spotfleet sample

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-variable "profile" { }
-variable "region"  { }
+variable "name"           { }
+variable "profile"        { }
+variable "region"         { }
+variable "key_name"       { }
+variable "prices"         { type = "map" }
+variable "instance_types" { type = "map" }
+variable "instance_type"  { }
+variable "vpc_id"         { }
 
 provider "aws" {
   profile = "${var.profile}"
@@ -7,6 +13,83 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "spotfleet_role" {
-  name = "aws-ec2-spot-fleet-role"
-  assume_role_policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"spotfleet.amazonaws.com\"},\"Action\":\"sts:AssumeRole\"}]}"
+  name               = "spotfleet-role"
+  assume_role_policy = "${file("assume_role_policy.json")}"
+}
+
+resource "aws_iam_policy_attachment" "spotfleet_role" {
+  name       = "EC2SpotFleetRole"
+  roles      = ["${aws_iam_role.spotfleet_role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetRole"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
+
+resource "aws_security_group" "fleet-sample-sg" {
+  name        = "${var.name}-sg"
+  vpc_id      = "${var.vpc_id}"
+  description = "${var.name}-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_spot_fleet_request" "fleet" {
+  iam_fleet_role      = "${aws_iam_role.spotfleet_role.arn}"
+  spot_price          = "${var.prices["default"]}"
+  allocation_strategy = "lowestPrice"
+  target_capacity     = 1
+  valid_until         = "2018-01-01T00:00:00Z"
+
+  launch_specification {
+    instance_type               = "${var.instance_types["${var.instance_type}"]}"
+    ami                         = "${data.aws_ami.amazon_linux.id}"
+    key_name                    = "${var.key_name}"
+    spot_price                  = "${var.prices["${var.instance_type}"]}"
+    vpc_security_group_ids      = ["${aws_security_group.fleet-sample-sg.id}"]
+    associate_public_ip_address = true
+
+    root_block_device {
+      volume_size = "8"
+      volume_type = "gp2"
+    }
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,17 @@
+name    = "fleet-sample"
 profile = "bob"
 region  = "ap-northeast-1"
+key_name = "myec2key"
+
+prices  = {
+  "default"   = "0.067"
+  "m3_medium" = "0.03"
+  "c3_large"  = "0.04"
+}
+
+instance_types = {
+  "m3_medium" = "m3.medium"
+  "c3_large"  = "c3.large"
+}
+
+vpc_id = "vpc-0586736d"
