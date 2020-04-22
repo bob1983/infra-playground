0e918808c5a0a94d0bae5db35e02ca6a3696c265
bob1983 <bob.1983.g@gmail.com>
2020-04-22 23:50:09 +0900

    Fetch table info

--- dynamodb-sandbox/src/table-operations.js
+++ dynamodb-sandbox/src/table-operations.js
@@ -3,8 +3,14 @@ AWS.config.update({ region: 'ap-northeast-1' })
 
 const dynamodb = new AWS.DynamoDB()
 
+/* Retrieve table info */
 dynamodb.listTables().promise().then(data => {
-  console.log(data)
+  const detailOps = data['TableNames'].map((tableName) => {
+    return dynamodb.describeTable({ TableName: tableName }).promise()
+  })
+  return Promise.all(detailOps)
+}).then(tableDetails => {
+  console.log(tableDetails)
 }).catch(error => {
   console.error(error)
 })
