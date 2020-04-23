d49050ba3d54cdb9b6eeb2be4832a7d4b862abaa
bob1983 <bob.1983.g@gmail.com>
2020-04-23 23:20:32 +0900

    Conditional write expamle

--- dynamodb-sandbox/src/table-operations.js
+++ dynamodb-sandbox/src/table-operations.js
@@ -4,16 +4,16 @@ AWS.config.update({ region: 'ap-northeast-1' })
 const dynamodb = new AWS.DynamoDB()
 
 /* Retrieve table info */
-dynamodb.listTables().promise().then(data => {
-  const detailOps = data['TableNames'].map((tableName) => {
-    return dynamodb.describeTable({ TableName: tableName }).promise()
-  })
-  return Promise.all(detailOps)
-}).then(tableDetails => {
-  console.log('TableDetails: ', tableDetails)
-}).catch(error => {
-  console.error(error)
-})
+// dynamodb.listTables().promise().then(data => {
+//   const detailOps = data['TableNames'].map((tableName) => {
+//     return dynamodb.describeTable({ TableName: tableName }).promise()
+//   })
+//   return Promise.all(detailOps)
+// }).then(tableDetails => {
+//   console.log('TableDetails: ', tableDetails)
+// }).catch(error => {
+//   console.error(error)
+// })
 
 const documentClient = new AWS.DynamoDB.DocumentClient()
 
@@ -33,57 +33,108 @@ const documentClient = new AWS.DynamoDB.DocumentClient()
 // })
 
 /* Update item */
-documentClient.update({
-  TableName: 'user_posts',
-  Key: {
-    user_id: 'dajfkdyieua11',
-    timestamp: 1
-  },
-  UpdateExpression: 'set #t = :t',
-  ExpressionAttributeNames: {
-    '#t': 'title'
-  },
-  ExpressionAttributeValues: {
-    ':t': 'Updated title'
-  }
-}).promise().then(data => {
-  console.log('Update: ', data)
-}).catch(error => {
-  console.error('Update error: ', error)
-})
+// documentClient.update({
+//   TableName: 'user_posts',
+//   Key: {
+//     user_id: 'dajfkdyieua11',
+//     timestamp: 1
+//   },
+//   UpdateExpression: 'set #t = :t',
+//   ExpressionAttributeNames: {
+//     '#t': 'title'
+//   },
+//   ExpressionAttributeValues: {
+//     ':t': 'Updated title'
+//   }
+// }).promise().then(data => {
+//   console.log('Update: ', data)
+// }).catch(error => {
+//   console.error('Update error: ', error)
+// })
 
-/* Batch operation */
+/* Batch write */
+// documentClient
+//   .batchWrite({
+//     RequestItems: {
+//       user_posts: [
+//         {
+//           PutRequest: {
+//             Item: {
+//               user_id: "foo1",
+//               timestamp: 100,
+//               title: "Foo title",
+//               content: "Foo content",
+//             },
+//           },
+//           PutRequest: {
+//             Item: {
+//               user_id: "bar",
+//               timestamp: 101,
+//               title: "Bar title",
+//               content: "Bar content",
+//             },
+//           },
+//         },
+//       ],
+//     },
+//   })
+//   .promise()
+//   .then((data) => {
+//     console.log("BatchUpdate: ", data);
+//   })
+//   .catch((error) => {
+//     console.error("BatchUpdate error: ", error);
+//   })
 
-documentClient
-  .batchWrite({
-    RequestItems: {
-      user_posts: [
-        {
-          PutRequest: {
-            Item: {
-              user_id: "foo1",
-              timestamp: 100,
-              title: "Foo title",
-              content: "Foo content",
-            },
-          },
-          PutRequest: {
-            Item: {
-              user_id: "bar",
-              timestamp: 101,
-              title: "Bar title",
-              content: "Bar content",
-            },
-          },
-        },
-      ],
-    },
-  })
-  .promise()
-  .then((data) => {
-    console.log("BatchUpdate: ", data);
-  })
-  .catch((error) => {
-    console.error("BatchUpdate error: ", error);
-  });
+/* Conditional write */
 
+// documentClient.put({
+//   TableName: 'user_posts',
+//   Item: {
+//     user_id: 'baz1',
+//     timestamp: 102,
+//     title: 'Conditional write title',
+//     content: 'Conditional write content'
+//   },
+//   ConditionExpression: '#t <> :t',
+//   ExpressionAttributeNames: {
+//     '#t': 'timestamp'
+//   },
+//   ExpressionAttributeValues: {
+//     ':t': 102
+//   }
+// })
+//   .promise()
+//    .then((data) => {
+//     console.log("Conditional write: ", data);
+//   })
+//   .catch((error) => {
+//     console.error("Conditional write error: ", error);
+//   })
+
+// documentClient
+//   .put({
+//     TableName: "user_posts",
+//     Item: {
+//       user_id: "baz1",
+//       timestamp: 102,
+//       title: "Conditional write not write title",
+//       content: "Conditional write not write content",
+//     },
+//     ConditionExpression: "#t <> :t",
+//     ExpressionAttributeNames: {
+//       "#t": "timestamp",
+//     },
+//     ExpressionAttributeValues: {
+//       ":t": 102,
+//     },
+//   })
+//   .promise()
+//   .then((data) => {
+//     console.log("Conditional write: ", data);
+//   })
+//   .catch((error) => {
+//     // Conditional Writeに失敗する(すでに timestamp 102 のItemが存在するので)
+//     // 失敗時には例外がThrowされる
+//     console.error("Conditional write error: ", error);
+//   });
