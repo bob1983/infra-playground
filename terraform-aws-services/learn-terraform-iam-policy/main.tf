258f3c21aa89a2ae6c76f1272cf7e6b062fa2c0e
bobstrange <bob.1983.g@gmail.com>
2021-06-04 23:30:12 +0900

    refactor: refactor aws_iam_policy_document
    ---
    
    Use, `aws_iam_policy_document` data source to define iam policy.

--- terraform-aws-services/learn-terraform-iam-policy/main.tf
+++ terraform-aws-services/learn-terraform-iam-policy/main.tf
@@ -30,31 +30,23 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+data "aws_iam_policy_document" "s3-all" {
+  statement {
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.bucket.arn]
+    effect = "Allow"
+  }
+}
+
 resource "aws_iam_policy" "policy" {
   name        = "${random_pet.pet_name.id}-policy"
   description = "My test policy"
 
-  policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:ListAllMyBuckets"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.bucket.arn}"
-    }
-
-  ]
-}
-EOT
+  policy = data.aws_iam_policy_document.s3-all.json
 }
 
